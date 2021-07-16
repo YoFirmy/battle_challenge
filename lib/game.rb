e56c30e514427296ebb7bd7dba0345c14c2f15d2
Matt Firmston <mattfirmston@gmail.com>
2021-07-16 11:30:06 +0100

    added switch turns functionality and changed views to display both players HP

--- lib/game.rb
+++ lib/game.rb
@@ -1,13 +1,25 @@
 class Game
   attr_reader :player_1
   attr_reader :player_2
+  attr_reader :current_turn
 
   def initialize(player_1, player_2)
     @player_1 = player_1
     @player_2 = player_2
+    @current_turn = @player_1
   end
 
   def attack(player)
     player.receive_damage
+  end
+
+  def switch_turns
+    @current_turn = opponent
+  end
+
+  private
+
+  def opponent
+    @current_turn == @player_1 ? @player_2 : @player_1
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -3,4 +3,9 @@ feature 'Hit points' do
     sign_in_and_play
     expect(page).to have_content "Kirk's HP: 100"
   end
+
+  scenario 'players can view their own hit points' do
+    sign_in_and_play
+    expect(page).to have_content "Picard's HP: 100"
+  end
 end
