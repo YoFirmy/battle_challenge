5f9f2132613d698e87750a2e782267b8cd473c93
Matt Firmston <mattfirmston@gmail.com>
2021-07-16 10:23:43 +0100

    saved players to game model to be called from there instead

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,13 +1,26 @@
 require 'game'
 
 describe Game do
-  let(:picard) { double :player }
-  let(:kirk) { double :player }
+  let(:player_1) { double :player }
+  let(:player_2) { double :player }
+  subject(:game) { described_class.new(player_1, player_2) }
 
   describe '#attack' do
     it 'damages the player' do
-      expect(kirk).to receive(:receive_damage)
-      subject.attack(kirk)
+      expect(player_2).to receive(:receive_damage)
+      game.attack(player_2)
+    end
+  end
+
+  describe '#player_1' do
+    it "retrieves the first player" do
+      expect(game.player_1).to eq(player_1)
+    end
+  end
+
+  describe '#player_2' do
+    it "retrieves the second player" do
+      expect(game.player_2).to eq(player_2)
     end
   end
 end
